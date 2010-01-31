09e79899977b764fd5ecd56a0f263b333ad63d65
Peeter P. Mõtsküla <ppm@taskr.eu>
2010-01-31 21:54:15 +0200

    ZF barebones application created -- application, model, controllers, actions, layout

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,46 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function resetAction()
+    {
+        // action body
+    }
+
+    public function signupAction()
+    {
+        // action body
+    }
+
+    public function emailAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
