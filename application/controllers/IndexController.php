d53529431c023aade7260e08198b99770ec34a1e
Peeter P. Mõtsküla <ppm@taskr.eu>
2010-02-05 22:07:32 +0200

    Index controller and view script for the welcome page

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,20 +1,27 @@
 <?php
-
+/**
+ * @package Taskr
+ * @author Peeter P. Mõtsküla <ppm@taskr.eu>
+ * @todo copyright & license
+ * @version 0.1.0
+ *
+ */
+/**
+ * Index controller
+ */
 class IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
+    /**
+     * If the user is logged in, forwards to Task controller;
+     * otherwise, the welcome page will be shown.
+     */
     public function indexAction()
     {
         if (Zend_Auth::getInstance()->hasIdentity()) {
-            $this->_helper->Redirector->gotoSimple('index', 'task');
+            $this->_forward('index', 'task');
         }
     }
-
 
 }
 
